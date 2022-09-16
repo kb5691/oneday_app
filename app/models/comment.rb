e5431f6d5eadd77c3b6fa8712cd9dd6f7ef7f087
oshiro <kaito.o.0628@gmail.com>
2022-09-16 13:07:14 +0900

    モデルの関連付けとバリデーションの追加

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+  # contentカラムの入力を必須にする
+  validates :content, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,12 @@
 class User < ApplicationRecord
+  # userが複数のpostを持つように設定し、ユーザーが削除されたら投稿も削除される
+  has_many :posts, dependent: :destroy
+  # userが複数のlikeを持つように設定し、ユーザーが削除されたらいいねも削除される
+  has_many :likes, dependent: :destroy
+  # userが複数のcommentを持つように設定し、ユーザーが削除されたらコメントも削除される
+  has_many :comments, dependent: :destroy
+  # userが「いいね」しているメッセージの一覧を取得できるように設定
+  has_many :liked_posts, through: :likes, source: :post
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
