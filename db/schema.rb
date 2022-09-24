e219f9783a3bc68681ecc70147f3c3dc1ec4e427
oshiro <kaito.o.0628@gmail.com>
2022-09-24 11:45:32 +0900

    コメント機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_11_122752) do
+ActiveRecord::Schema.define(version: 2022_09_21_224918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2022_09_11_122752) do
     t.string "recommend_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
