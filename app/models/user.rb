16541866abfb4ba40d49efbc844ece23aa170610
oshiro <kaito.o.0628@gmail.com>
2022-09-30 09:05:12 +0900

    ゲストログイン用のボタンを設置

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,7 @@ class User < ApplicationRecord
   # ゲストアカウントを取得する
   def self.guest
     find_or_create_by!(email: "guest@example.com") do |user|
+      user.name = "ゲスト"
       user.password = SecureRandom.urlsafe_base64
     end
   end
