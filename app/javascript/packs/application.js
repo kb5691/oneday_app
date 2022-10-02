fc4acdba48845bd65b4a86dd7af508eb92c36d7b
oshiro <kaito.o.0628@gmail.com>
2022-10-02 16:34:44 +0900

    google map機能用のjavascriptファイルを追加しリファクタリング

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,12 +7,12 @@ import Rails from '@rails/ujs';
 import Turbolinks from 'turbolinks';
 import * as ActiveStorage from '@rails/activestorage';
 import 'channels';
-
-// chart.js
 import Chart from 'chart.js/auto';
-global.Chart = Chart;
-require('./graph');
 
 Rails.start();
 Turbolinks.start();
 ActiveStorage.start();
+
+// chart.js
+global.Chart = Chart;
+require('./graph');
