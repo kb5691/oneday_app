d0023c9f422961cdb33e9be1c00da3a32c8ae75f
oshiro <kaito.o.0628@gmail.com>
2022-10-16 19:07:08 +0900

    ログイン前のトップページのレイアウトを修正

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
+  skip_before_action :authenticate_user!, only: %i[index]
 
   # 1ページの表示数
   PER_PAGE = 1
