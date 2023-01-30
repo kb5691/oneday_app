2697e277d3fa491ce92eeb1f8b2eadf463aac521
oshiro <kaito.o.0628@gmail.com>
2023-01-30 20:53:39 +0900

    それぞれのモデルに対応したfactory_botを追加

--- spec/factories/posts.rb
+++
@@ -1,9 +0,0 @@
-FactoryBot.define do
-  factory :post do
-    title { Faker::Lorem.word }
-    recommend_point { Faker::Name.name }
-    situation { Faker::Food.dish }
-    area { Faker::Address.city }
-    user
-  end
-end
